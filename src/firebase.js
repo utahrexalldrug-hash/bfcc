09adfe594f8182a194310c203f67335f3f41559e
utahrexalldrug-hash <utahrexalldrug@gmail.com>
2026-02-15 21:23:05 -0700

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAKnJO89fSQKr7kuaKhkxtc15BFNqpLDtQ",
+  authDomain: "family-hq-c133c.firebaseapp.com",
+  projectId: "family-hq-c133c",
+  storageBucket: "family-hq-c133c.firebasestorage.app",
+  messagingSenderId: "59125881260",
+  appId: "1:59125881260:web:615713de1add487fbb0209"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
